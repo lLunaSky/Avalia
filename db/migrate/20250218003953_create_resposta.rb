ad75d36715b7df04e5414e5b5a88ab377bc530fe
❥ giulia <giuliamferreira01@gmail.com>
2025-02-17 22:10:07 -0300

    Update 20250218003953_create_resposta.rb

--- db/migrate/20250218003953_create_resposta.rb
+++ db/migrate/20250218003953_create_resposta.rb
@@ -1,6 +1,6 @@
-class CreateResposta < ActiveRecord::Migration[8.0]
+class CreateRespostas < ActiveRecord::Migration[8.0]
   def change
-    create_table :resposta do |t|
+    create_table :respostas do |t|  # <-- Corrigido para plural
       t.references :avaliacao, null: false, foreign_key: true
       t.string :pergunta
       t.string :resposta
