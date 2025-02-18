cb4da9ade4969c06c3384f7bf240a6a32848c225
❥ giulia <giuliamferreira01@gmail.com>
2025-02-17 23:03:19 -0300

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,25 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require 'json'
+
+# Caminho do arquivo JSON
+file_path = Rails.root.join('db', 'classes.json')
+
+if File.exist?(file_path)
+  file = File.read(file_path)
+  avaliacoes = JSON.parse(file)
+
+  puts "Inserindo avaliações no banco..."
+
+  avaliacoes.each do |avaliacao|
+    Avaliacao.find_or_create_by!(codigo: avaliacao["code"]) do |a|
+      a.nome = avaliacao["name"]
+      a.semestre = avaliacao["class"]["semester"]
+    end
+  end
+
+  puts "Avaliações importadas com sucesso!"
+else
+  puts "Arquivo JSON não encontrado!"
+end
