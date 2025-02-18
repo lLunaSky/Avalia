4b8fec1ba572c3e8b69b13d0d75eedda17950ec4
❥ giulia <giuliamferreira01@gmail.com>
2025-02-17 22:39:59 -0300

    Update avaliacao.rb

--- app/models/avaliacao.rb
+++ app/models/avaliacao.rb
@@ -1,4 +1,4 @@
 class Avaliacao < ApplicationRecord
-  self.table_name = "avaliacoes" # Garante que Rails reconheça a tabela certa
-  has_many :respostas
+  self.table_name = "avaliacoes"
+  has_many :respostas, dependent: :destroy
 end
