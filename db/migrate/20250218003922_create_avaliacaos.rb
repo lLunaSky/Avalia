0d68c5ccb8031b22c9e763ec9b4d3be87dd089f3
❥ giulia <giuliamferreira01@gmail.com>
2025-02-17 22:09:58 -0300

    Update 20250218003922_create_avaliacaos.rb

--- db/migrate/20250218003922_create_avaliacaos.rb
+++ db/migrate/20250218003922_create_avaliacaos.rb
@@ -1,6 +1,6 @@
-class CreateAvaliacaos < ActiveRecord::Migration[8.0]
+class CreateAvaliacoes < ActiveRecord::Migration[8.0]
   def change
-    create_table :avaliacaos do |t|
+    create_table :avaliacoes do |t|  # <-- Corrigido para plural
       t.string :nome
       t.string :semestre
       t.string :codigo
